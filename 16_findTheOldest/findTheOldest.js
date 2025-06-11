eb7561a143945136776dc241dfb55a7f024af7c6
Anthony <106450012+AnthonyBAC@users.noreply.github.com>
2025-06-11 17:50:29 -0400

    16/16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const getAge = function(birth, death){
+    if (!death){
+        death = new Date().getFullYear()
+    }
+    return death - birth
 };
+
+
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, currentPerson) => {
+        const oldtAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath)
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath)
+        return oldtAge < currentAge ? currentPerson : oldestPerson;
+            });
+    };
 
 // Do not edit below this line
 module.exports = findTheOldest;
